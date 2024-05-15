33087cb07db36ae99ae508bff153c6b04807081d
riachx <ria.fvista@gmail.com>
2024-05-15 00:54:32 -0700

    Updates

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const material = new THREE.MeshPhysicalMaterial({
         reflectivity: 0.2,
 });
 
-const planeTexture = new THREE.TextureLoader().load("/host-physical-material/background.png");
+const planeTexture = new THREE.TextureLoader().load("background.png");
 const plane_mat = new THREE.MeshBasicMaterial({ map: planeTexture });
 
 const plane_geo = new THREE.BoxGeometry(2, 2, 0.1);
@@ -30,7 +30,7 @@ mesh.castShadow = true; // Enable the object to cast shadows
 scene.add(mesh);
 
 const rgbeLoader = new RGBELoader();
-  rgbeLoader.load('../belfast.hdr', function (texture) {
+  rgbeLoader.load('belfast.hdr', function (texture) {
     texture.mapping = THREE.EquirectangularReflectionMapping;
     renderer.toneMapping = THREE.CineonToneMapping;
     renderer.toneMappingExposure = 1;
